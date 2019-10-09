36ffaaecfc07bf7e914110f8006b9ad41505e786
3hhosaini@gmail.com <hasanmahtab>
2019-10-09 21:59:11 +0330

    Initial commit

---
+++ logutil/src/main/java/ir/techninja/logutil/LogDebug.java
@@ -0,0 +1,12 @@
+package ir.techninja.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    public static final String TAG = "debugTag";
+
+    public static void d(String message){
+        Log.e(TAG, "Bingooooo.....");
+    }
+}
